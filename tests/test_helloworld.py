20cf0ed0489b527579ec6755547f57997405f21a
polarbear08 <polaris10377@gmail.com>
2023-03-04 17:15:57 +0900

    #00012-UPD hello worldを表示するモジュールとテストを追加

---
+++ tests/test_helloworld.py
@@ -0,0 +1,10 @@
+import unittest
+from maxcript.greeting import helloworld
+
+class TestPrintHelloWorldTest(unittest.TestCase):
+    def test_print_hello_world(self):
+        self.assertEqual(helloworld.print_hello_world(), "hello world")
+        
+if __name__ == '__main__':
+    unittest.main()
+    
